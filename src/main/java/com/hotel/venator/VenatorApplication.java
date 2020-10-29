080cb0383505f75964254121bc87e17876e98540
Admin <Admin@DESKTOP-G2QUO26>
2020-10-29 15:01:23 +0200

    Init project

---
+++ src/main/java/com/hotel/venator/VenatorApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.venator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/venator/VenatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.venator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
