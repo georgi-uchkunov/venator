f0413bd96d4ec9465e9008e1328179823b7854c6
Admin <Admin@DESKTOP-G2QUO26>
2021-06-21 15:02:05 +0300

    Client side validation part 1

--- src/main/resources/static/javascript/booking.js
+++ src/main/resources/static/javascript/booking.js
@@ -47,4 +47,133 @@ $(function(){
 			});
 	
 	})
+
+	$("#given-name").on('change', function () {
+		var $givenName = $("#given-name");
+		var $givenNameFeedback = $("#given-name-feedback");
+		var givenName = $givenName.val();
+		if (givenName.length > 0 && givenName != 'name') {
+			$givenName[0].classList.remove('is-invalid');
+			$givenName[0].classList.add('is-valid');
+			$givenNameFeedback[0].css({
+				'display': 'none'
+			})
+		} else {
+			$givenName[0].classList.remove('is-valid');
+			$givenName[0].classList.add('is-invalid');
+			$givenNameFeedback[0].css({
+				'display': 'block'
+			})
+	
+		}
+	})
+
+	$("#family-name").on('change', function () {
+		var $familyName = $("#family-name");
+		var $familyNameFeedback = $("#family-name-feedback");
+		var familyName = $familyName.val();
+		if (familyName.length > 0 && familyName != 'name') {
+			$familyName[0].classList.remove('is-invalid');
+			$familyName[0].classList.add('is-valid');
+			$familyNameFeedback[0].css({
+				'display': 'none'
+			})
+		} else {
+			$familyName[0].classList.remove('is-valid');
+			$familyName[0].classList.add('is-invalid');
+			$familyNameFeedback[0].css({
+				'display': 'block'
+			})
+	
+		}
+	})
+
+	$("#reservation-email").on('change', function () {
+		var $email = $("#reservation-email");
+		var $emailFeedback = $("#reservation-email-feedback");
+		var email = $email.val();
+		var validation = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+		if (validation.test(email)) {
+			$email[0].classList.remove('is-invalid');
+			$email[0].classList.add('is-valid');
+			$emailFeedback[0].css({
+				'display': 'none'
+			})
+		} else {
+			$email[0].classList.remove('is-valid');
+			$email[0].classList.add('is-invalid');
+			$emailFeedback[0].css({
+				'display': 'block'
+			})
+		}
+	})
+
+	$("#phone-number").on('change', function () {
+		var $countryCode = $("#country-code");
+		var $phoneNumber = $("#phone-number");
+		var fullNumber = $countryCode.val() + $phoneNumber.val();
+		var $phoneNumberFeedback = $("#phone-number-feedback");
+		var validation = /^\d{10}$/;
+		if (validation.test(fullNumber)) {
+			$phoneNumber[0].classList.remove('is-invalid');
+			$phoneNumber[0].classList.add('is-valid');
+			$phoneNumberFeedback[0].css({
+				'display': 'none'
+			})
+		} else {
+			$phoneNumber[0].classList.remove('is-valid');
+			$phoneNumber[0].classList.add('is-invalid');
+			$phoneNumberFeedback[0].css({
+				'display': 'block'
+			})
+		}
+	})
+
+	$("#name-card").on('change', function () {
+		var $nameCard = $("#name-card");
+		var $nameCardFeedback = $("#name-card-feedback");
+		var nameCard = $nameCard.val();
+		if (nameCard.length > 0 && nameCard != 'name') {
+			$nameCard[0].classList.remove('is-invalid');
+			$nameCard[0].classList.add('is-valid');
+			$nameCardFeedback[0].css({
+				'display': 'none'
+			})
+		} else {
+			$nameCard[0].classList.remove('is-valid');
+			$nameCard[0].classList.add('is-invalid');
+			$nameCardFeedback[0].css({
+				'display': 'block'
+			})
+	
+		}
+	})
+
+	$("#card-number").on('change', function () {
+		var $cardNumber = $("#card-number");
+		var $cardNumberFeedback = $("#card-number-feedback");
+		var cardNumber = $cardNumber.val();
+		var mastercardValidation = /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/;
+		var americanExpressValidation = /^3[47][0-9]{13}$/;
+		var visaValidation = /^4[0-9]{12}(?:[0-9]{3})?$/;
+		var discoverValidation = /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/;
+		var maestroValidation = /^(5018|5081|5044|5020|5038|603845|6304|6759|676[1-3]|6799|6220|504834|504817|504645)[0-9]{8,15}$/;
+		var jcbValidation = /^(?:2131|1800|35[0-9]{3})[0-9]{11}$/;
+		var dinersClubValidation = /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/;
+		if (mastercardValidation.test(cardNumber) || americanExpressValidation.test(cardNumber) || visaValidation.test(cardNumber)
+		|| discoverValidation.test(cardNumber) || maestroValidation.test(cardNumber) || jcbValidation.test(cardNumber)
+		|| dinersClubValidation.test(cardNumber)) {
+			$cardNumber[0].classList.remove('is-invalid');
+			$cardNumber[0].classList.add('is-valid');
+			$cardNumberFeedback[0].css({
+				'display': 'none'
+			})
+		} else {
+			$cardNumber[0].classList.remove('is-valid');
+			$cardNumber[0].classList.add('is-invalid');
+			$cardNumberFeedback[0].css({
+				'display': 'block'
+			})
+		}
+	})
 })
